1c3212acd0389b3907316f71f9e1d86fe57b5c1a
César Magalhães <ceanma83@gmail.com>
2021-10-10 20:47:23 -0300

    Configuração para criação de um cluster kubernetes versão 1.22.2 no container centos/7 do LXC

---
+++ lxc/centos7-k8s1.22.2/bootstrap-k8s.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+echo "[TASK 01] Configure static IP"
+cat > /etc/sysconfig/network-scripts/ifcfg-eth0 <<EOF
+TYPE=Ethernet
+BOOTPROTO=none
+IPADDR=192.168.100.111
+PREFIX=24
+GATEWAY=192.168.100.1
+DNS1=192.168.100.1
+DNS2=8.8.8.8
+DNS3=8.8.4.4
+DEFROUTE=yes
+IPV4_FAILURE_FATAL=no
+IPV6INIT=no
+NAME=eth0
+DEVICE=eth0
+ONBOOT=yes
+EOF
+systemctl restart network
+
+##Update the OS and install yum-utils, bash completion, git, and more
+echo "[TASK 02] Update the OS and install yum-utils, bash completion, git, and more"
+yum update -y >/dev/null 2>&1 
+yum install yum-utils nfs-utils bash-completion git -y -q >/dev/null 2>&1
+ 
+##Disable firewall starting from Kubernetes v1.19 onwards
+#systemctl disable firewalld --now 
+ 
+## letting ipTables see bridged networks
+echo "[TASK 03] Letting ipTables see bridged networks"
+cat > /etc/modules-load.d/k8s.conf <<EOF
+br_netfilter
+EOF
+
+cat > /etc/sysctl.d/k8s.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system >/dev/null 2>&1
+ 
+##
+## iptables config as specified by CRI-O documentation
+# Create the .conf file to load the modules at bootup
+echo "[TASK 04] Create the .conf file to load the modules at bootup"
+cat > /etc/modules-load.d/crio.conf <<EOF
+overlay
+br_netfilter
+EOF
+ 
+#modprobe overlay
+#modprobe br_netfilter
+ 
+# Set up required sysctl params, these persist across reboots.
+echo "[TASK 05] Set up required sysctl params, these persist across reboots"
+cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+ 
+sysctl --system >/dev/null 2>&1
+ 
+###
+## Configuring Kubernetes repositories
+echo "[TASK 06] Add yum repo file for kubernetes"
+cat > /etc/yum.repos.d/kubernetes.repo <<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+ 
+## Set SELinux in permissive mode (effectively disabling it)
+#setenforce 0
+#sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+ 
+### Disable swap
+#swapoff -a
+ 
+##make a backup of fstab
+#cp -f /etc/fstab /etc/fstab.bak
+ 
+##Renove swap from fstab
+#sed -i '/swap/d' /etc/fstab
+ 
+##Refresh repo list
+echo "[TASK 07] Refresh repo list"
+yum repolist -y >/dev/null 2>&1
+
+echo "[TASK 08] Install CRI-O" 
+## Install CRI-O binaries
+##########################
+ 
+#Operating system   $OS
+#Centos 8   CentOS_8
+#Centos 8 Stream    CentOS_8_Stream
+#Centos 7   CentOS_7
+ 
+#set OS version
+OS=CentOS_7 
+#set CRI-O
+VERSION=1.22
+ 
+# Install CRI-O
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo >/dev/null 2>&1
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo >/dev/null 2>&1
+yum install cri-o -y -q >/dev/null 2>&1
+
+echo "[TASK 09] Enable CRI-O"
+systemctl daemon-reload
+systemctl enable crio --now >/dev/null 2>&1
+ 
+##Install Kubernetes, specify Version as CRI-O
+echo "[TASK 10] Install Kubernetes (kubeadm, kubelet and kubectl)"
+yum install -y -q kubelet-$VERSION.2-0 kubeadm-$VERSION.2-0 kubectl-$VERSION.2-0 --disableexcludes=kubernetes >/dev/null 2>&1
+
+# Start and Enable kubelet service
+echo "[TASK 11] Enable and start kubelet service"
+systemctl enable kubelet >/dev/null 2>&1
+echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/sysconfig/kubelet
+systemctl start kubelet
+
+# Install Openssh server
+echo "[TASK 12] Install and configure ssh"
+yum install -y -q openssh-server >/dev/null 2>&1
+sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl enable sshd
+systemctl start sshd
+
+# Set Root password
+echo "[TASK 11] Set root password"
+echo "abc12345" | passwd --stdin root >/dev/null 2>&1
+
+# Install additional required packages
+echo "[TASK 12] Install additional packages"
+yum install -y -q which net-tools sudo sshpass less >/dev/null 2>&1
+
+# Hack required to provision K8s v1.15+ in LXC containers
+echo "[TASK 13] Hack required to provision K8s v1.15+ in LXC containers"
+mknod /dev/kmsg c 1 11
+chmod +x /etc/rc.d/rc.local
+echo 'mknod /dev/kmsg c 1 11' >> /etc/rc.d/rc.local
+
+echo "[TASK 14] Update /etc/hosts file"
+cat >> /etc/hosts <<EOF
+192.168.100.100   k8s.cams7.ml
+192.168.100.101   lb101.cams7-vaio.local
+192.168.100.102   lb102.cams7-dell.local
+192.168.100.103   lb103.cams7-qbex.local
+192.168.100.111   kmaster111.cams7-vaio.local
+192.168.100.112   kmaster112.cams7-vaio.local
+192.168.100.113   kmaster113.cams7-vaio.local
+192.168.100.114   kmaster114.cams7-dell.local
+192.168.100.115   kmaster115.cams7-dell.local
+192.168.100.116   kmaster116.cams7-dell.local
+192.168.100.117   kmaster117.cams7-qbex.local
+192.168.100.118   kmaster118.cams7-qbex.local
+192.168.100.119   kmaster119.cams7-qbex.local
+192.168.100.121   kworker121.cams7-vaio.local
+192.168.100.122   kworker122.cams7-vaio.local
+192.168.100.123   kworker123.cams7-vaio.local
+192.168.100.124   kworker124.cams7-dell.local
+192.168.100.125   kworker125.cams7-dell.local
+192.168.100.126   kworker126.cams7-dell.local
+192.168.100.127   kworker127.cams7-qbex.local
+192.168.100.128   kworker128.cams7-qbex.local
+192.168.100.129   kworker129.cams7-qbex.local
+192.168.100.130   traefik.cams7.local
+192.168.100.105   nfs.cams7.local
+EOF
+
+#######################################
+# To be executed only on master nodes #
+#######################################
+
+if [[ $(hostname) =~ .*master.* ]]
+then
+  echo "[TASK 15] Pull required containers"
+  kubeadm config images pull >/dev/null 2>&1
+fi
+
+if [[ $(hostname) =~ .*master111.* ]]
+then
+
+  # Initialize Kubernetes
+  echo "[TASK 16] Initialize Kubernetes Cluster"
+  kubeadm init  --control-plane-endpoint="k8s.cams7.ml:6443" --upload-certs --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all >> /root/kubeinit.log 2>&1
+
+  # Copy Kube admin config
+  echo "[TASK 17] Copy kube admin config to root user .kube directory"
+  mkdir /root/.kube
+  cp /etc/kubernetes/admin.conf /root/.kube/config
+
+  # Deploy flannel network
+  echo "[TASK 18] Deploy flannel network"
+  kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml > /dev/null 2>&1
+
+fi
+
+
---
+++ lxc/centos7-k8s1.22.2/bootstrap-lb.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+echo "[TASK 01] Configure static IP"
+cat > /etc/network/interfaces <<EOF
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet static
+  hostname $(hostname)
+  address 192.168.100.101
+  netmask 24
+  network 192.168.100.0
+  gateway 192.168.100.1
+EOF
+ip addr flush eth0 
+ifdown eth0 
+ifup eth0
+
+# Install Openssh server
+echo "[TASK 02] Install and configure ssh"
+apk update >/dev/null 2>&1 
+apk add --no-cache openssh >/dev/null 2>&1 
+rc-update add sshd >/dev/null 2>&1
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config 
+service sshd restart >/dev/null 2>&1
+
+# Set Root password
+echo "[TASK 03] Set root password"
+echo -e "abc12345\nabc12345" | passwd root >/dev/null 2>&1 
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+echo "[TASK 04] Update /etc/hosts file"
+cat >> /etc/hosts <<EOF
+192.168.100.100   k8s.cams7.ml
+192.168.100.101   lb101.cams7-vaio.local
+192.168.100.102   lb102.cams7-dell.local
+192.168.100.103   lb103.cams7-qbex.local
+192.168.100.111   kmaster111.cams7-vaio.local
+192.168.100.112   kmaster112.cams7-vaio.local
+192.168.100.113   kmaster113.cams7-vaio.local
+192.168.100.114   kmaster114.cams7-dell.local
+192.168.100.115   kmaster115.cams7-dell.local
+192.168.100.116   kmaster116.cams7-dell.local
+192.168.100.117   kmaster117.cams7-qbex.local
+192.168.100.118   kmaster118.cams7-qbex.local
+192.168.100.119   kmaster119.cams7-qbex.local
+192.168.100.121   kworker121.cams7-vaio.local
+192.168.100.122   kworker122.cams7-vaio.local
+192.168.100.123   kworker123.cams7-vaio.local
+192.168.100.124   kworker124.cams7-dell.local
+192.168.100.125   kworker125.cams7-dell.local
+192.168.100.126   kworker126.cams7-dell.local
+192.168.100.127   kworker127.cams7-qbex.local
+192.168.100.128   kworker128.cams7-qbex.local
+192.168.100.129   kworker129.cams7-qbex.local
+192.168.100.130   traefik.cams7.local
+192.168.100.105   nfs.cams7.local
+EOF
+
+echo "[TASK 05] Install ucarp and haproxy"
+apk update >/dev/null 2>&1 
+apk add --no-cache ucarp haproxy >/dev/null 2>&1 
+
+echo "[TASK 06] Configure ucarp"
+ln -s /etc/init.d/ucarp /etc/init.d/ucarp.eth0
+cat > /etc/ucarp/ucarp.pass <<EOF
+abc12345
+EOF
+cat > /etc/conf.d/ucarp.eth0 <<EOF
+UCARP_INTERFACE=eth0
+UCARP_SOURCEADDRESS=192.168.100.101
+UCARP_VIRTUALADDRESS=192.168.100.100
+UCARP_VIRTUALPREFIX=24
+UCARP_VHID=123
+UCARP_PASSFILE=/etc/ucarp/ucarp.pass
+UCARP_UPSCRIPT=/etc/ucarp/vip-up-default.sh
+UCARP_DOWNSCRIPT=/etc/ucarp/vip-down-default.sh
+UCARP_OPTS="--shutdown"
+EOF
+chmod +x /etc/conf.d/ucarp.eth0
+rc-update add /etc/conf.d/ucarp.eth0 >/dev/null 2>&1 
+
+echo "[TASK 07] Configure haproxy"
+cat >> /etc/haproxy/haproxy.cfg <<EOF
+
+frontend traefik_http_bind
+    bind *:80
+#    bind [::]:80
+    mode tcp
+    option tcplog
+    default_backend router_traefik_http
+
+frontend traefik_https_bind
+    bind *:443
+#    bind [::]:443
+    mode tcp
+    option tcplog
+    default_backend router_traefik_https
+
+frontend k8s_bind 
+    bind *:6443 
+#    bind [::]:6443
+    mode tcp
+    option tcplog
+    default_backend router_k8s
+
+frontend nfs_bind 
+    bind *:2049 
+#    bind [::]:2049
+    mode tcp
+    option tcplog
+    default_backend router_nfs
+
+frontend postgres_bind
+    bind *:5432
+    mode tcp
+    option tcplog
+    default_backend router_postgres
+
+frontend mongodb_bind
+    bind *:27017
+    mode tcp
+    option tcplog
+    default_backend router_mongodb
+
+frontend redis_bind
+    bind *:6379
+    mode tcp
+    option tcplog
+    default_backend router_redis
+
+backend router_traefik_http
+    mode tcp
+    option tcp-check
+    server traefik traefik.cams7.local:80 check
+
+backend router_traefik_https
+    mode tcp
+    option tcp-check
+    server traefik traefik.cams7.local:443 check
+
+backend router_k8s
+    mode tcp 
+    option tcp-check 
+    balance roundrobin 
+    server kmaster111 kmaster111.cams7-vaio.local:6443 check fall 3 rise 2
+#    server kmaster112 kmaster112.cams7-vaio.local:6443 check fall 3 rise 2
+#    server kmaster113 kmaster113.cams7-vaio.local:6443 check fall 3 rise 2
+    server kmaster114 kmaster114.cams7-dell.local:6443 check fall 3 rise 2
+#    server kmaster115 kmaster115.cams7-dell.local:6443 check fall 3 rise 2
+#    server kmaster116 kmaster116.cams7-dell.local:6443 check fall 3 rise 2
+    server kmaster117 kmaster117.cams7-qbex.local:6443 check fall 3 rise 2
+#    server kmaster118 kmaster118.cams7-qbex.local:6443 check fall 3 rise 2
+#    server kmaster119 kmaster119.cams7-qbex.local:6443 check fall 3 rise 2
+
+backend router_nfs
+    mode tcp
+    option tcp-check
+    server nfs nfs.cams7.local:2049 check
+
+backend router_postgres
+    mode tcp
+    option tcp-check
+    server nfs traefik.cams7.local:5432 check
+
+backend router_mongodb
+    mode tcp
+    option tcp-check
+    server nfs traefik.cams7.local:27017 check
+
+backend router_redis
+    mode tcp
+    option tcp-check
+    server nfs traefik.cams7.local:6379 check
+EOF
+rc-update add haproxy >/dev/null 2>&1
