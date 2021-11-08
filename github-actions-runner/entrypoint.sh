68356395ca23103add0ddb30ef5599692d02afdd
César Magalhães <ceanma83@gmail.com>
2021-11-08 14:11:37 -0300

    Update github-actions-runner

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
 echo "Requesting registration URL at '${registration_url}'"
-payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PERSONAL_TOKEN}" ${registration_url})
+payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 ./config.sh \
     --name $(hostname) \
     --token ${RUNNER_TOKEN} \
     --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
-    --work /home/github-actions-runner \
+    --work ${RUNNER_WORKDIR} \
+    --labels "${RUNNER_LABELS}" \
     --unattended \
     --replace
 remove() {
